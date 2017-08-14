a08a872ea7a80641dde03acbd1374e361163044b
maksim <gtl.vorobyov@gmail.com>
2017-08-14 22:47:04 +0300

    -add

--- tools/JsonToCommands/main.js
+++ tools/JsonToCommands/main.js
@@ -1,40 +1,44 @@
 (function() {
-  const btn = document.querySelector('.btn');
+  const find = (selector, attr) => {
+    return document.querySelector(selector);
+  };
+
+  const btn = find('.btn');
 
   btn.addEventListener('click', e => {
     e.preventDefault();
-    const packageContentStr = document.querySelector('#objectData').value;
+    const packageContentStr = find('#objectData').value;
     const packageContent = JSON.parse(packageContentStr);
-    const bundlerAgent = document.querySelector('.radio:checked').value;
-    const dependenciesType = document.querySelector('.radio2:checked').value;
-    const commandLineField = document.getElementById('commands');
+    const bundlerAgent = find('.radio:checked').value;
+    const dependenciesType = find('.radio2:checked').value;
+    const commandsField = find('#commands');
 
-    commandLineField.textContent = jsonConverter(
+    commandsField.textContent = getCommands(
       bundlerAgent,
       dependenciesType,
       packageContent,
     );
 
-    commandLineField.addEventListener('focus', e => {
-      commandLineField.select();
+    commandsField.addEventListener('focus', e => {
+      commandsField.select();
     });
   });
 
-  const jsonConverter = (bundlerAgent, dependenciesType, packageContent) => {
+  const getCommands = (bundlerAgent, dependenciesType, packageContent) => {
     const isNPM = bundlerAgent === 'npm';
-    let commandLineStr = isNPM ? 'npm i' : 'yarn add';
+    let commandsStr = isNPM ? 'npm i' : 'yarn add';
 
     if (dependenciesType === 'devDependencies') {
-      commandLineStr += isNPM ? ' -D' : ' --dev';
+      commandsStr += isNPM ? ' -D' : ' --dev';
     }
 
     for (let key in packageContent) {
       if (key === dependenciesType) {
-        for (let field in packageContent[key]) {
-          commandLineStr += ' ' + field;
+        for (let value in packageContent[key]) {
+          commandsStr += ' ' + value;
         }
       }
     }
-    return commandLineStr;
+    return commandsStr;
   };
 })();
