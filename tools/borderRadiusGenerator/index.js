f1a00da53efcaec4bcd97f28b9a56205a33853ba
maksim <gtl.vorobyov@gmail.com>
2017-09-06 17:09:08 +0300

    -add

--- tools/borderRadiusGenerator/index.js
+++ tools/borderRadiusGenerator/index.js
@@ -2,7 +2,7 @@
   const divBlock = document.querySelector('.block');
   const inputRangeArray = document.querySelectorAll('.inpRange');
 
-  function setCornerValue(inputValue, inputId) {
+  function transferDataToVisualBlock(inputValue, inputId) {
     const cornersArray = document.querySelectorAll('.corner');
 
     [...cornersArray].forEach(corner => {
@@ -11,31 +11,31 @@
       }
     });
   }
-  function setDataToCopy() {
+  function transferDataToCopy() {
     const copyTextElement = document.querySelector('.dataValue');
     let copyText = '';
+
     [...inputRangeArray].forEach(inputRange => {
       copyText += inputRange.value + 'px ';
     });
-
     copyTextElement.textContent = copyText.slice(0, -1) + ';';
-    console.log(copyTextElement.textContent);
   }
 
-  function transferRangeValue() {
+  (function transferRangeToTextValue() {
     [...inputRangeArray].forEach(inputRange => {
       inputRange.addEventListener('input', e => {
-        const parentRow = inputRange.parentNode.parentNode;
+        const thisInput = e.target;
+        const parentRow = thisInput.parentNode.parentNode;
         const inputText = parentRow.querySelector('.inpText');
         const thisInputValue = e.target.value;
         inputText.value = thisInputValue;
         divBlock.style[inputRange.id] = thisInputValue + 'px';
-        setCornerValue(thisInputValue, inputRange.id);
-        setDataToCopy();
+        transferDataToVisualBlock(thisInputValue, inputRange.id);
+        transferDataToCopy();
       });
     });
-  }
-  function transferTextValue() {
+  })();
+  (function transferTextToRangeValue() {
     const maxValue = 150;
     const inpTextArray = document.querySelectorAll('.inpText');
 
@@ -50,10 +50,8 @@
         inputRange.value = thisInput.value === '' ? 0 : thisInput.value;
 
         divBlock.style[inputRange.id] = thisInput.value + 'px';
-        setCornerValue(thisInput.value, inputRange.id);
+        transferDataToVisualBlock(thisInput.value, inputRange.id);
       });
     });
-  }
-  transferTextValue();
-  transferRangeValue();
+  })();
 })();
