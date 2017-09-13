e2b381148cb63c09c5adc9f816466b7fdac5ff63
maksim <gtl.vorobyov@gmail.com>
2017-09-13 10:01:06 +0300

    -add

--- react-weather/weather/src/App.js
+++ react-weather/weather/src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 import { Navbar, NavItem, Nav, Grid, Row, Col } from 'react-bootstrap';
 
 const PLACES = [
-  { name: 'Hurzuf', zip: '707860' },
-  // {name: "San Jose", zip: "94040"},
-  // {name: "Santa Cruz", zip: "95062"},
-  // {name: "Reynosa", zip: "35203"}
+  { name: 'Makaha Valley', zip: '707860' },
+  {name: "Los Altos", zip: "94040"},
+  {name: "Twin Lakes", zip: "95062"},
+  {name: "Panevezys", zip: "35203"}
 ];
 
 class App extends Component {
@@ -92,7 +92,7 @@ class WeatherDisplay extends Component {
   componentDidMount() {
     const zip = this.props.zip;
     const URL =
-      'http://api.openweathermap.org/data/2.5/weather?id=' +
+      'http://api.openweathermap.org/data/2.5/weather?q=' +
       zip +
       '&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial';
     fetch(URL).then(res => res.json()).then(json => {
