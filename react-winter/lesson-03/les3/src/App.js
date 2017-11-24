211e60c8b3c6068d33166437714a8b1678d447cc
Maksym Vorobiov <maksim.vorobyov@bigdropinc.com>
2017-11-24 18:57:23 +0200

    -add

--- react-winter/lesson-03/les3/src/App.js
+++ react-winter/lesson-03/les3/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import ActualTask from './components/less_7_home/task_home_7_6'
+import ActualTask from './components/less_7_home/task_home_7_4_2'
 
 class App extends Component {
 
